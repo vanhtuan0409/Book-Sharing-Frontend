689f09805090b0af587b4a564435bf2261417917
tuanvuong <vanhtuan0409@gmail.com>
2015-08-05 10:23:38 +0900

    github commit for popup

--- app/views/header/header.js
+++ app/views/header/header.js
@@ -1,7 +1,9 @@
 'use strict';
 
-angular.module('myApp.header', [])
-
+angular.module('myApp.header', ['facebook'])
+.config(function(FacebookProvider){
+	FacebookProvider.init('869003383193808');
+})
 .directive('ngHeader', ['$jQueryLoader', '$timeout', function($jQueryLoader, $timeout) {
 	return {
 		restrict: 'E',
@@ -13,49 +15,27 @@ angular.module('myApp.header', [])
 				$jQueryLoader.loadDropdown(false);
 			})
 		},
-		controller: ['$scope', '$http', '$auth', '$appConfig', '$cookieStore', function($scope, $http, $auth, $appConfig, $cookies){
+		controller: ['$scope', '$http', '$auth', '$appConfig', '$cookieStore', 'Facebook', function($scope, $http, $auth, $appConfig, $cookies, Facebook){
 			$scope.user = $auth.getUser();
-
-			function fb_login(){
-				FB.login(function (response) {
-					if (response.authResponse) {
-			            var access_token = response.authResponse.accessToken;
-			            var id = response.authResponse.userID;
-			            $auth.login(access_token, id, function(){
-			            	window.location.reload();
-			            });
-			        }
-			    }, {
-			    	scope: ['email', 'public_profile', 'user_friends']
-			    });
-			}
-
-			 function statusChangeCallback(response) {
-				if (status === 'connected') {
-					var access_token = response.authResponse.accessToken;
-		            var id = response.authResponse.userID;
-		            $auth.login(access_token, id, function(){
-		            	window.location.reload();
-		            });
-				} else if (status === 'not_authorized') {
-					fb_login();
-				} else {
-					fb_login();
-				}
-			}
-
-			var status = null;
-			$scope.checkLoginState =function() {
-				FB.getLoginStatus(function(response) {
-					// statusChangeCallback(response);
-					status = response.status;
-				});
-			}
 
 			$scope.logout = function(){
 				$auth.logout();
 				window.location.reload();
 			}
+
+			$scope.login = function() {
+				Facebook.login(function(response) {
+					if (response.authResponse) {
+						var access_token = response.authResponse.accessToken;
+						var id = response.authResponse.userID;
+						$auth.login(access_token, id, function(){
+							window.location.reload();
+						});
+					}
+				}, {
+			    	scope: ['email', 'public_profile', 'user_friends']
+			    });
+			};
 		}]
 	}
 }]);
